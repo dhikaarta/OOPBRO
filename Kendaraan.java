58db1313a38e3c43c436e1ec9f79dd747ac84460
dhikaarta <16520019@std.stei.itb.ac.id>
2022-03-30 21:25:27 +0700

    masukin file

---
+++ Kendaraan.java
@@ -0,0 +1,8 @@
+abstract class Kendaraan{
+    protected int nomorKendaraan;
+    protected String kategori;
+    protected String merk; 
+    protected int tahunKeluaran;
+
+    abstract void cetakInfo();
+}
